75be103baef80776643ef6505c60121033e102f2
joao.costa@hole19golf.com <joao.costa@hole19golf.com>
2013-08-10 22:42:50 +0100

    Changes http server to respond with static files if they're in the public folder

--- my_http_server.js
+++ my_http_server.js
@@ -7,6 +7,8 @@ app.use(function (req, res, next) {
   console.log('%s %s', req.method, req.url);
   next();
 });
+
+app.use(express.static(__dirname + '/public'));
 app.listen(3000);
 
 console.log('Listening on port 3000');
