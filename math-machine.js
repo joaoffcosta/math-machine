ea7fc915e60743ccc71881ed8c834d775062581c
joao.costa@hole19golf.com <joao.costa@hole19golf.com>
2013-08-10 23:08:38 +0100

    Adds parsing of values and sets of values to MathMachine

--- math-machine.js
+++ math-machine.js
@@ -1,5 +1,22 @@
+function value(value, rest) {
+  return rest == null ? value : [value].concat(rest);
+}
+
 function machine(operation) {
-  return 27;
+  if (operation == "")
+    return null;
+
+  console.log("Parsing: " + operation)
+
+  var first = operation.split("/", 1)[0];
+  var rest  = operation.substring(first.length + 1);
+
+  // is this a value?
+  if (first.match(/^\d+$/)) {
+    return value(parseInt(first), machine(rest));
+  }
+  
+  return null;
 }
 
 exports.machine = machine;
