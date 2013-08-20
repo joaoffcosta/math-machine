d6cc36eac3476ee1c2bd6853bd530e69ff761b7f
João Costa <joaostacosta@gmail.com>
2013-08-20 23:22:30 +0100

    Refactors math-machine operations

--- math-machine.js
+++ math-machine.js
@@ -2,59 +2,29 @@ function value(value, rest) {
   return rest == null ? value : [value].concat(rest);
 }
 
-// Unary operations
-
-function negation(arg) {
-  return -arg;
-}
-
-function absolute(arg) {
-  return Math.abs(arg);
-}
-
-// Binary operations
-
-function power(arg1, arg2) {
-  return Math.pow(arg1, arg2);
-}
-
-function squareRoot(arg1, arg2) {
-  return Math.sqrt(arg1, arg2);
-}
-
-// N-ary operations
-
-function addition(args) {
-  var value = args[0];
-  for (var i = 1; i < args.length; i++) {
-    value += args[i];
+function applyNaryOperation(numbers, operation) {
+  var value = numbers[0];
+  for (var i = 1; i < numbers.length; i++) {
+    value = operation(value, numbers[i]);
   }
   return value;
 }
 
-function subtraction(args) {
-  var value = args[0];
-  for (var i = 1; i < args.length; i++) {
-    value -= args[i];
-  }
-  return value;
-}
+op = { }
 
-function multiplication(args) {
-  var value = args[0];
-  for (var i = 1; i < args.length; i++) {
-    value *= args[i];
-  }
-  return value;
-}
+// unary
+op.neg = function(num) { return -num; }
+op.abs = function(num) { return Math.abs(num); }
 
-function division(args) {
-  var value = args[0];
-  for (var i = 1; i < args.length; i++) {
-    value /= args[i];
-  }
-  return value;
-}
+// binary
+op.pow  = function(base, exp)  { return Math.pow(base, exp); }
+op.sqrt = function(rad, deg)   { return Math.sqrt(rad, deg); }
+
+// n-ary
+op.add = function(numbers) { return applyNaryOperation(numbers, function(n1,n2){return n1+n2;}); }
+op.sub = function(numbers) { return applyNaryOperation(numbers, function(n1,n2){return n1-n2;}); }
+op.mul = function(numbers) { return applyNaryOperation(numbers, function(n1,n2){return n1*n2;}); }
+op.div = function(numbers) { return applyNaryOperation(numbers, function(n1,n2){return n1/n2;}); }
 
 function machine(operation) {
   if (operation == "")
@@ -74,22 +44,22 @@ function machine(operation) {
 
   // unary operators
   switch (first) {
-    case "neg":  return value(negation(args[0]), args.slice(1));
-    case "abs":  return value(absolute(args[0]), args.slice(1));
+    case "neg":  return value(op.neg(args[0]), args.slice(1));
+    case "abs":  return value(op.abs(args[0]), args.slice(1));
   }
 
   // binary operators
   switch (first) {
-    case "pow":  return value(power(     args[0], args[1]), args.slice(2));
-    case "sqrt": return value(squareRoot(args[0], args[1]), args.slice(2));
+    case "pow":  return value(op.pow( args[0], args[1]), args.slice(2));
+    case "sqrt": return value(op.sqrt(args[0], args[1]), args.slice(2));
   }
 
   // n-ary operators
   switch (first) {
-    case "add": return addition(args);
-    case "sub": return subtraction(args);
-    case "mul": return multiplication(args);
-    case "div": return division(args);
+    case "add": return op.add(args);
+    case "sub": return op.sub(args);
+    case "mul": return op.mul(args);
+    case "div": return op.div(args);
   }
   
   return null;
