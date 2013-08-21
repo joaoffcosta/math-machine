81b3b68dafa14fd6a820983eaad835a1dd14e240
João Costa <joaostacosta@gmail.com>
2013-08-22 00:20:28 +0100

    Implements new (heap-based) version of the Math Machine

---
+++ machine.js
@@ -0,0 +1,47 @@
+var ops = require('./operations.js').ops;
+
+var operands = [];
+var values   = [];
+
+function callOp(op) {
+  if (op.args == -1) {
+    var args = values.reverse();
+    values   = [];
+    return op.func(args);
+
+  } else if (op.args == 1) {
+    return op.func(values.pop());
+
+  } else if (op.args == 2) {
+    return op.func(values.pop(), values.pop());
+  }
+}
+
+function parse(operation) {
+  if (operation == "")
+    return null;
+
+  operands = operation.split("/");
+  values   = [];
+  
+  var operand = undefined;
+  while (operand = operands.pop()) {
+    console.log("[" + values + "] << " + operand);
+
+    if (operand.match(/^\d+$/)) {
+      values.push(parseInt(operand));
+
+    } else if (ops[operand]) {
+      var value = callOp(ops[operand]);
+      values.push(value);
+
+    } else {
+      console.log("ERROR: Unrecognized operand: " + operand);
+    }
+  }
+
+  console.log("[" + values + "]");
+  return values.reverse();
+};
+
+exports.parse = parse;
---
+++ operations.js
@@ -0,0 +1,51 @@
+function opify(args, func) {
+  return {
+    args: args,
+    func: func
+  };
+}
+
+function nAry(numbers, op) {
+  var value = numbers[0];
+  for (var i = 1; i < numbers.length; i++)
+    value = op(value, numbers[i]);
+  return value;
+}
+
+// unary
+function neg(num)  { return -num; }
+function abs(num)  { return Math.abs(num); }
+function fact(num) { return num <= 1 ? 1 : num * arguments.callee(num - 1) }
+
+// binary
+function add(num1, num2) { return num1 + num2; }
+function sub(num1, num2) { return num1 - num2; }
+function mul(num1, num2) { return num1 * num2; }
+function div(num1, num2) { return num1 / num2; }
+function pow(base, exp)  { return Math.pow(base, exp); }
+function sqrt(rad, deg)  { return Math.sqrt(rad, deg); }
+
+// n-ary
+function addn(numbers) { return nAry(numbers, add); }
+function subn(numbers) { return nAry(numbers, sub); }
+function muln(numbers) { return nAry(numbers, mul); }
+function divn(numbers) { return nAry(numbers, div); }
+
+
+exports.ops = {
+  neg:  opify(1, neg),
+  abs:  opify(1, abs),
+  fact: opify(1, fact),
+  
+  add:  opify(2, add),
+  sub:  opify(2, sub),
+  mul:  opify(2, mul),
+  div:  opify(2, div),
+  pow:  opify(2, pow),
+  sqrt: opify(2, sqrt),
+  
+  addn: opify(-1, addn),
+  subn: opify(-1, subn),
+  muln: opify(-1, muln),
+  divn: opify(-1, divn),
+};
