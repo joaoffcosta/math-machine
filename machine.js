ff205928967ac2e717d582a85ab514d5dbbd23fb
João Costa <joaostacosta@gmail.com>
2013-08-26 22:50:01 +0100

    Implements end token

--- machine.js
+++ machine.js
@@ -3,11 +3,24 @@ var ops = require('./operations.js').ops;
 var operands = [];
 var values   = [];
 
+function removeEndTokens() {
+  var index;
+  while((index = values.indexOf("end")) != -1) {
+    values.splice(index, 1);
+  }
+}
+
+function popUntilEnd() {
+  var res = [], val;
+  while((val = values.pop()) != undefined && val != "end") {
+    res.push(val);
+  }
+  return res;
+}
+
 function callOp(op) {
   if (op.args == -1) {
-    var args = values.reverse();
-    values   = [];
-    return op.func(args);
+    return op.func(popUntilEnd());
 
   } else if (op.args == 1) {
     return op.func(values.pop());
@@ -31,6 +44,9 @@ function parse(operation) {
     if (operand.match(/^\d+$/) != undefined) {
       values.push(parseInt(operand));
 
+    } else if (operand == "end") {
+      values.push(operand);
+
     } else if (ops[operand] != undefined) {
       var value = callOp(ops[operand]);
       values.push(value);
@@ -40,6 +56,7 @@ function parse(operation) {
     }
   }
 
+  removeEndTokens();
   console.log("[" + values + "]");
   return values.reverse();
 };
